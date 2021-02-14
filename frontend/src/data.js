fd511a4877f40e54258824f4ddc9d7a3ca2ea9a3
hasanfuad <43511316+hasanfuad@users.noreply.github.com>
2021-02-14 12:33:33 +0600

    commit sidebar and dynamic product

---
+++ frontend/src/data.js
@@ -0,0 +1,47 @@
+export default {
+    products: [{
+        name: "Slim Shirt",
+        category: "Shirt",
+        images: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.5,
+        reviews: 10
+    },
+    {
+        name: "Fit Shirt",
+        category: "Shirt",
+        images: "/images/d1.jpg",
+        price: 50,
+        brand: "Cats Eye",
+        rating: 4.6,
+        reviews: 9
+    },
+    {
+        name: "Best Pants",
+        category: "Pants",
+        images: "/images/d1.jpg",
+        price: 70,
+        brand: "Lotto",
+        rating: 4.8,
+        reviews: 15
+    },
+    {
+        name: "Best Pants",
+        category: "Pants",
+        images: "/images/d1.jpg",
+        price: 70,
+        brand: "Lotto",
+        rating: 4.8,
+        reviews: 15
+    },
+    {
+        name: "Best Pants",
+        category: "Pants",
+        images: "/images/d1.jpg",
+        price: 70,
+        brand: "Lotto",
+        rating: 4.8,
+        reviews: 15
+    }]
+}
